36500d60bb5eddea80b6d187ee2db96d29a0cdf5
Gausi86 <gauravsingh.2386@gmail.com>
2025-12-27 07:24:27 +0000

    commit6

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import seaborn as sns
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # -------------------- IMPORT PROJECT MODULES --------------------
 from data.stock_data import fetch_stock_data
